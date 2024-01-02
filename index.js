796c4849bc45b81a4dd52eb19bdae1e39efbb18d
florida-12 <formula120877@gmail.com>
2024-01-02 17:34:05 -0500

    Updated styles, added a warrior page in classes, added a footer constructor

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs/promises');
 const app = express();
 
+let footer_html;
+
+fs.readFile(path.join(__dirname, 'views/footer.ejs'), 'utf-8')  // Чтение содержимого файла при запуске сервера
+    .then(content => {
+        footer_html = content;
+    })
+    .catch(error => {
+        console.error('Error reading file:', error);
+        process.exit(1);  // Выход с кодом ошибки, если чтение не удалось
+    }
+);
 
 app.use(express.static(path.join(__dirname, 'assets')));
 
@@ -10,11 +22,15 @@ app.set('views', path.join(__dirname, 'views'));
 
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', { footer: footer_html });
 });
 
 app.get('/classes', (req, res) => {
-    res.render('classes');
+    res.render('classes', { footer: footer_html });
+});
+
+app.get('/classes/warrior', (req, res) => {
+    res.render('classes/warrior', { footer: footer_html });
 });
 
 
