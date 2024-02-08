663570ef9ab7d2f01b73151410ff904a3087948c
florida-12 <formula120877@gmail.com>
2024-02-08 10:36:47 -0500

    Added items page, added page for adding items

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs/promises');
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
 
-let footer_html;
+const db = new sqlite3.Database('database.db');
+db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, image TEXT, rarity TEXT, level INTEGER, data TEXT)");
+});
 
+let footer_html;
 fs.readFile(path.join(__dirname, 'views/footer.ejs'), 'utf-8')  // Чтение содержимого файла при запуске сервера
     .then(content => {
         footer_html = content;
@@ -13,9 +21,34 @@ fs.readFile(path.join(__dirname, 'views/footer.ejs'), 'utf-8')  // Чтение 
         console.error('Error reading file:', error);
         process.exit(1);  // Выход с кодом ошибки, если чтение не удалось
     }
-);
+    );
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'media/items/');
+    },
+    filename: function (req, file, cb) {
+        const extension = path.extname(file.originalname);
+        const uniqueFilename = uuidv4() + extension;
+        cb(null, uniqueFilename);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'assets')));
+
+app.use((req, res, next) => {
+    req.body = removeEmptyFields(req.body);
+    next();
+});
+
+function removeEmptyFields(obj) {
+    return Object.fromEntries(
+        Object.entries(obj).filter(([key, value]) => value !== '' && value !== null && value !== undefined)
+    );
+}
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -43,6 +76,70 @@ app.get('/classes/paladin', (req, res) => {
 
 app.get('/classes/thief', (req, res) => {
     res.render('classes/thief', { footer: footer_html });
+});
+
+app.get('/items', (req, res) => {
+    if (req.query.level == 'above') {
+        db.all("SELECT * FROM items ORDER BY -level;", (err, rows) => {
+            if (err) {
+                return res.status(500).send("Error retrieving data from database.");
+            }
+    
+            res.render('items', { items: rows, url: req.url, footer: footer_html });
+        });
+    } else if (req.query.level == 'below') {
+        db.all("SELECT * FROM items ORDER BY level;", (err, rows) => {
+            if (err) {
+                return res.status(500).send("Error retrieving data from database.");
+            }
+    
+            res.render('items', { items: rows, url: req.url, footer: footer_html });
+        });
+    } else {
+        db.all("SELECT * FROM items;", (err, rows) => {
+            if (err) {
+                return res.status(500).send("Error retrieving data from database.");
+            }
+    
+            res.render('items', { items: rows, url: req.url, footer: footer_html });
+        });
+    }
+});
+
+app.get('/items/add', (req, res) => {
+    res.render('add-item', { footer: footer_html });
+});
+
+app.post('/items/add', upload.single('image'), (req, res) => {
+    if (req.body.password != 'bJ9?0v6@7m|C%<k£}e') return res.redirect('/items/add');
+    if (!req.file) return res.status(400).send('No file uploaded.');
+
+    const { name, rarity, level } = req.body;
+    const image = req.file.filename;
+    delete req.body.name; delete req.body.image; delete req.body.rarity; delete req.body.level; delete req.body.password;
+    let data = JSON.stringify(req.body);
+
+    const insertStatement = db.prepare("INSERT INTO items (name, image, rarity, level, data) VALUES (?, ?, ?, ?, ?)");
+    insertStatement.run(name, image, rarity, level, data, function (err) {
+        if (err) {
+            return res.status(500).send("Error inserting user into database.");
+        }
+    });
+    insertStatement.finalize();
+
+    res.redirect('/items/add');
+});
+
+app.get('/media/items/:imageName', async (req, res) => {
+    const imageName = req.params.imageName;
+    const imagePath = path.join(__dirname, 'media\\items', imageName);
+
+    try {
+        await fs.access(imagePath);
+        res.sendFile(imagePath);
+    } catch (error) {
+        res.status(404).send('Image not found.');
+    }
 });
 
 
